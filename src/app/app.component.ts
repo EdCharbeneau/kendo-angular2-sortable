ad5f4c3d4d3dd898f7d39cf5c628f945a0b87256
Ed Charbeneau <ed.charbeneau@gmail.com>
2016-11-28 15:23:58 -0500

    Databound sortables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Sortable Demo';
+
+  groups = [];
+  ungroupedContacts = [];
+
+  constructor() {
+    this.groups = this.getContacts()
+                      .filter(c => c.group != "Ungrouped");
+      
+    this.ungroupedContacts = this.getContacts()
+                                 .filter(c => c.group == "Ungrouped")[0].people;
+  }
+  private getContacts() {
+    return [
+      {
+        group: "Friends",
+        people: [
+          {
+            id: 6,
+            name: "Todd Motto",
+            location: "London"
+          },
+          {
+            id: 9,
+            name: "Ed Charbeneau",
+            location: "Louisville"
+          }
+        ]
+      },
+      {
+        group: "Teams",
+        people: [
+          {
+            id: 3,
+            name: "Mary Jane",
+            location: "Sofia"
+          },
+          {
+            id: 4,
+            name: "Linda Belcher",
+            location: "Orlando"
+          }
+        ]
+      },
+      {
+        group: "Ungrouped",
+        people: [
+          {
+            id: 1,
+            name: "Sarah Smith",
+            location: "New York"
+          },
+          {
+            id: 2,
+            name: "Ron Burgundy",
+            location: "Boston"
+          },
+          {
+            id: 5,
+            name: "Morty Smith",
+            location: "Boston"
+          },
+          {
+            id: 7,
+            name: "Rick Sanchez",
+            location: "Boston"
+          }
+        ]
+      }
+    ];
+  }
 }
